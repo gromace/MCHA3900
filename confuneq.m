edd15c6ecf5c12613698dc192bce1a619690f435
Chloe Warner <Chloe.Warner@uon.edu.au>
2018-05-27 10:11:52 +1000

    Put into own folder

--- confuneq.m
+++
@@ -1,5 +0,0 @@
-function [c, ceq] = confuneq(in)
-% Nonlinear equality constraints
-c = [];
-ceq = sqrt(in(1)^2 + in(2)^2 + in(3)^2) - 1;
-end
--- objfun.m
+++
@@ -1,4 +0,0 @@
-function f = objfun(in)
-f = sqrt(in(1)^2 + in(2)^2 + in(3)^2)
-end
-
