daf9ffa738eb6d2ad1afe137a9e349839c7ce67c
Werehoef <39557943+Werehoef@users.noreply.github.com>
2018-06-01 03:11:17 +1000

    update model

--- Question 3/measurementLikelihood_3.m
+++ Question 3/measurementLikelihood_3.m
@@ -9,8 +9,8 @@ load('Initial_image_pose.mat') %Use for POSE just for random data?
 %POSE=[N E D; theta phi psi; dtheta dphi dpsi];
 
 load('calibration_sample_vector_points.mat')
-
-
+N=100;
+p.vc = 0.5*randn(N,1)+1;
 p.vec1 = rHCc_norm(:,1);
 p.vec2 = rHCc_norm(:,2);
 
@@ -49,6 +49,16 @@ assert(P_is + P_pl  + P_null == 1, 'Prior probabilities must sum to 1');
 theta = [sigma_cex_is;sigma_cex_pl;P_is;P_pl;P_null];
 
 %% Automatic parameter tuning
+p.thetacb= 0.2;
+p.thetanb= 0.2;
+p.vCBb= 0;
+p.vBNb= 0;
+p.omegaBNb= 0 ;
+p.RPNn= [1 0 0;0 1 0;0 0 1];
+p.RBNn= [1 0 0;0 1 0;0 0 1];
+p.rCbb= [1;1;1];
+p.rQCc= rHCc_norm;
+
 
 % Find optimal parameters
 theta = TuningLikelihood_3(theta, modelType, p);
